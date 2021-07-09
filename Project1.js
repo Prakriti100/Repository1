0ed4f72dece6a0c46409be6145c1eb9a75df3200
Prakriti100 <86748089+Prakriti100@users.noreply.github.com>
2021-07-09 19:10:37 -0400

    Add files via upload

--- Project1.js
+++ Project1.js
@@ -1,39 +1,29 @@
+var box
 
 function setup() {
   createCanvas(400,400);
+  background(51);
 
-  var btn_red = createButton("RED");
-  btn_red.position(100,50);
-  btn_red.mousePressed(red_bg);
-  if (mousePressed(btn_red)){
-    background("Red");
-  }
-
-  var btn_green = createButton("GREEN");
-  btn_green.position(250,50);
-  btn_green.mousePressed(green_bg);
-  if (mousePressed(btn_green)){
-    background("Green")
-  }
-
+box = createSprite(200,200,30,30);
 }
 
 
-function red_bg(){
-  r = 255;
-  g = 0;
-  b = 0;
-}
-function green_bg(){
-  r = 0;
-  g = 255;
-  b = 0;
-}
 
 
 
 function draw() {
- background(30);
+ if (keyIsDown(RIGHT_ARROW)){
+   background("Red");
+ }
+ if (keyIsDown(LEFT_ARROW)){
+  background("Blue");
+}
+if (keyIsDown(UP_ARROW)){
+  background("Yellow");
+}
+if (keyIsDown(DOWN_ARROW)){
+  background("Green");
+}
 
  drawSprites();
 }
